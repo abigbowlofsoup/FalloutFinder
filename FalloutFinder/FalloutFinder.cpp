7630d27e9b28537939470b81a1c5e87c7eae5018
AJ Brown <chargers5583@gmail.com>
2019-02-28 16:04:09 -0800

    added using statements and converted v_armor to a unique_ptr that points to a vector of armor on the heap

--- FalloutFinder/FalloutFinder.cpp
+++ FalloutFinder/FalloutFinder.cpp
@@ -2,37 +2,46 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <memory>
 #include "Armor.h"
 
-#define container std::vector< std::vector<std::string> > 
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+
+
+#define container vector< vector<string> > 
 
 void SkipBOM(std::ifstream &in);
 container ProcessFile(std::ifstream &in, int i_type);
 
-void UpdateArmorVec(std::vector<Armor> &v_output, container const &v_input);
+void UpdateArmorVec(std::unique_ptr<vector<Armor>>& v_output, container const &v_input);
 
-int main() {
+int main(int argc, char* argv[]) {
       
    //-----------------File I/O-----------------
    // Opening a .csv file for information
    std::ifstream input("../Armor.csv");
    if (!input) {
-      std::cerr << "Could not open the file!" << std::endl;
+      std::cerr << "Could not open the file!" << endl;
       return 0;
    }
 
    // Creates a 2d vector of info from our file
    // and creates an Armor vector with our items
    container processed = ProcessFile(input, 0);
-   std::vector<Armor> v_armor;
+   auto v_armor = std::make_unique<vector<Armor>>();
+   //std::unique_ptr<vector<Armor>> v_armor(vector<Armor>);
+
    UpdateArmorVec(v_armor, processed);
 
    //-----------------I/O-----------------
-   std::cout << "Welcome to Fallout Finder!\n"
-      << "--------------------------" << std::endl << ">";
+   cout << "Welcome to Fallout Finder!\n"
+      << "--------------------------" << endl << ">";
    
    // From StackOverflow
-   /*for (std::string line; std::getline(std::cin, line); ) {
+   /*for (string line; std::getline(std::cin, line); ) {
       
       if (line.empty()) { continue; }
 
@@ -47,18 +56,18 @@ int main() {
 
       if (line[0] == 'E' || line[0] == 'e') { break; }
 
-      std::cout << "Sorry, I did not understand.\n>";
+      cout << "Sorry, I did not understand.\n>";
    }*/
 
-   std::cout << "Goodbye!\n";
+   cout << "Goodbye!\n";
    
 
    
 
-   /*for (int i = 0; i < v_armor.size(); ++i) {
-      v_armor[i].PrintAll();
-      std::cout << "---------------" << std::endl;
-   }*/
+   for (int i = 0; i < (*v_armor).size(); ++i) {
+      (*v_armor)[i].PrintAll();
+      cout << "---------------" << endl;
+   }
 
    input.close();
    std::cin.get();
@@ -85,14 +94,14 @@ container ProcessFile(std::ifstream &in, int i_type) {
    SkipBOM(in);
    container v_obj;
    // Standard variables in every Item
-   std::string _name, _value, _weight, _baseid, _itemhp;
+   string _name, _value, _weight, _baseid, _itemhp;
 
    // Get rid of column names in csv
-   std::string dummy;
+   string dummy;
    std::getline(in, dummy);
 
    while (!in.eof()) {
-      std::vector<std::string> v_row;
+      vector<string> v_row;
 
       // Add all items to our 1d vector v_row for later
       std::getline(in, _name, ',');
@@ -108,20 +117,20 @@ container ProcessFile(std::ifstream &in, int i_type) {
       
       // If it is armor we add the damage rating variable
       if(i_type == 0){ 
-         std::string _dmg_r;
+         string _dmg_r;
          std::getline(in, _dmg_r, '\n'); 
          v_row.push_back(_dmg_r);
       }
 
       // If it is a weapon we add multiple variables
       if (i_type == 1) {
-         std::string _dmg_att;
+         string _dmg_att;
          std::getline(in, _dmg_att, ',');
-         std::string _dmg_proj;
+         string _dmg_proj;
          std::getline(in, _dmg_proj, ',');
-         std::string _att_sec;
+         string _att_sec;
          std::getline(in, _att_sec, ',');
-         std::string _crit_dmg;
+         string _crit_dmg;
          std::getline(in, _crit_dmg, '\n');
       }
       
@@ -133,11 +142,11 @@ container ProcessFile(std::ifstream &in, int i_type) {
 }
 
 // Adds our items to the Armor vector
-void UpdateArmorVec(std::vector<Armor> &v_output, container const &v_input) {
+void UpdateArmorVec(std::unique_ptr<vector<Armor>>& v_output, container const &v_input) {
    for (int r = 0; r < v_input.size(); ++r) {
       // Create Armor instances out of 2d vector
       // and push it into vector
       Armor var(v_input[r]);
-      v_output.push_back(var);
+      v_output->push_back(var);
    }
 }
